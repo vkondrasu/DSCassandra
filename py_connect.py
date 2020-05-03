257eb68541b20cc9b12627e562ebc578844a50e3
venkateswar.kondrasu <venkateswar.kondrasu@servicenow.com>
2020-05-03 09:35:01 +0530

    Inserting a row into videos_by_tag

--- py_connect.py
+++ py_connect.py
@@ -18,3 +18,6 @@ session.execute(""" DELETE FROM videos_by_tag WHERE tag='venkat_kondrasu' """);
 print('{0:12} {1:40} {2:5}'.format('Tag', 'ID', 'Title'))
 for val in session.execute("select * from videos_by_tag"):
    print('{0:12} {1:40} {2:5}'.format(val[0], val[2], val[3]))
+
+session.execute(""" INSERT INTO videos_by_tag (tag, video_id, added_date, title) 
+VALUES ('cassandra', uuid(), toUnixTimestamp(now()), 'cassandra certification video') """);
